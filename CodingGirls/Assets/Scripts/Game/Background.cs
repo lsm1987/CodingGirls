eb82eff7b9f424877607558c40e55c614ef7a152
lsm1987 <1987lsm@gmail.com>
2018-09-29 20:32:48 +0900

    [배경] 화면 비율이 배경 이미지와 달라도 배경이 화면을 가득 채우도록 수정

--- CodingGirls/Assets/Scripts/Game/Background.cs
+++ CodingGirls/Assets/Scripts/Game/Background.cs
@@ -22,6 +22,37 @@ namespace Game
         {
             _matColor = new Material(_matColorOri);
             _matTexture = new Material(_matTextureOri);
+            InitializeScale();
+        }
+
+        private void InitializeScale()
+        {
+            float camRatio = Camera.main.aspect;
+            float camHeight = Camera.main.orthographicSize * 2.0f;
+            float camWidth = camHeight * camRatio;
+
+            float oriWidth = transform.localScale.x;
+            float oriHeight = transform.localScale.y;
+            float oriRatio = oriWidth / oriHeight;
+
+            float scale = 1.0f;
+            if (camRatio > oriRatio)
+            {
+                // 화면의 가로 비율이 원본 배경 가로 비율보다 큼
+                // 예: 아이폰X = 19.5:9, 배경 = 16:9
+                // 원본 배경 가로가 화면에 맞도록 원본 스케일 조정
+                scale = camWidth / oriWidth;
+            }
+            else
+            {
+                // 예: 아이패드 프로 = 4:3, 배경 = 16:9
+                scale = camHeight / oriHeight;
+            }
+
+            float scaledWidth = oriWidth * scale;
+            float scaledHeight = oriHeight * scale;
+
+            transform.localScale = new Vector3(scaledWidth, scaledHeight, 1.0f);
         }
 
         public Texture LoadTexture(string textureName)
