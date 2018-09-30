75331061540ac2b119feb3e55dc950afb1f51b00
lsm1987 <1987lsm@gmail.com>
2018-09-30 15:31:36 +0900

    [메뉴] 게임 메뉴 아이콘이 Safe area 안에 위치하도록 수정

--- CodingGirls/Assets/Scripts/Game/UIMenu.cs
+++ CodingGirls/Assets/Scripts/Game/UIMenu.cs
@@ -7,6 +7,28 @@ namespace Game
         [SerializeField]
         private GameObject _container;
 
+        private void Awake()
+        {
+            MoveAnchorToSafeArea();
+        }
+
+        private void MoveAnchorToSafeArea()
+        {
+            // 우상단이 Safe Area 우상단과 일치하도록 이동
+            // 예) iPhone X
+            // Screen w:2436 h:1125
+            // SafeArea x:132.00, y:63.00, width:2172.00, height:1062.00
+
+            Vector2 rightTopAnchor = new Vector2(
+                (Screen.safeArea.x + Screen.safeArea.width) / (float)Screen.width,
+                1.0f - (Screen.safeArea.y / (float)Screen.height)
+            );
+
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            rectTransform.anchorMin = rightTopAnchor;
+            rectTransform.anchorMax = rightTopAnchor;
+        }
+
         public void OnOptionClicked()
         {
             ToggleContainerActive();
