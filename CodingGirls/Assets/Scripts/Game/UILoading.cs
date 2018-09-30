731be2f8fc08a980fd4da43168de9d5f5ec45861
lsm1987 <1987lsm@gmail.com>
2018-09-30 20:20:25 +0900

    [로딩] 로딩 텍스트 기준점이 Safe area 우하단이 되도록 수정

---
+++ CodingGirls/Assets/Scripts/Game/UILoading.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class UILoading : UIWindow
+    {
+        private void Awake()
+        {
+            MoveAnchorToSafeArea();
+        }
+
+        private void MoveAnchorToSafeArea()
+        {
+            // 우하단이 Safe Area 우하단과 일치하도록 이동
+            Vector2 rightBottomAnchor = new Vector2(
+                (Screen.safeArea.x + Screen.safeArea.width) / (float)Screen.width,
+                Screen.safeArea.y / (float)Screen.height
+            );
+
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            rectTransform.anchorMin = rightBottomAnchor;
+            rectTransform.anchorMax = rightBottomAnchor;
+        }
+    }
+}
